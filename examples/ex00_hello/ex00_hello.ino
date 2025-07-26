92f70739b820d266c55fab64a115205c810de21d
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-07-26 11:48:46 -0400

    agregar examples/

---
+++ examples/ex00_hello/ex00_hello.ino
@@ -0,0 +1,19 @@
+// ex00_hello
+// by montoyamoraga
+// july 2025
+
+// this example tests if the library is working
+
+// import library
+#include "FrecuenciasMIDI.h"
+
+// declare instance
+FrecuenciasMIDI frecuenciasMIDI;
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
