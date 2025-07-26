3a302aa3ec5920b7e3082c47843c35499c6ee2e7
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-07-26 11:45:18 -0400

    agregar templates de .h y .cpp

---
+++ src/FrecuenciasMIDI.cpp
@@ -0,0 +1,11 @@
+#include "FrecuenciasMIDI.h"
+
+// constructor
+FrecuenciasMIDI::FrecuenciasMIDI() {
+    
+}
+
+// destructor
+FrecuenciasMIDI::~FrecuenciasMIDI() {
+    
+}
---
+++ src/FrecuenciasMIDI.h
@@ -0,0 +1,13 @@
+#ifndef FRECUENCIAS_MIDI_H
+#define FRECUENCIAS_MIDI_H
+
+Class FrecuenciasMIDI {
+    public:
+        // constructor
+        FrecuenciasMIDI();
+
+        // destructor
+        ~FrecuenciasMIDI();
+};
+
+#endif
